813053967c6487232cb0dd5e6b9399c323e96eee
Steve Bauman <steven_bauman_7@hotmail.com>
2015-03-09 16:59:33 -0400

    Version bump

--- src/Stevebauman/Inventory/InventoryServiceProvider.php
+++ src/Stevebauman/Inventory/InventoryServiceProvider.php
@@ -15,7 +15,7 @@ class InventoryServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '1.3.0';
+    const VERSION = '1.3.1';
 
 	/**
 	 * Stores the package configuration separator
