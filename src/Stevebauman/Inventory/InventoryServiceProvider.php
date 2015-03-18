915001a0c8e7f9654cdfd8073dd99267f669acaa
Steve Bauman <steven_bauman@outlook.com>
2015-03-18 13:53:45 -0400

    Service provider formatting changes

--- src/Stevebauman/Inventory/InventoryServiceProvider.php
+++ src/Stevebauman/Inventory/InventoryServiceProvider.php
@@ -11,116 +11,119 @@ use Illuminate\Support\ServiceProvider;
 class InventoryServiceProvider extends ServiceProvider
 {
     /**
-     * Inventory version
-     *
-     * @var string
-     */
+    * Inventory version
+    *
+    * @var string
+    */
     const VERSION = '1.4.0';
 
-	/**
-	 * Stores the package configuration separator
-	 * for Laravel 5 compatibility
-	 *
-	 * @var string
-	 */
-	public static $packageConfigSeparator = '::';
+    /**
+    * Stores the package configuration separator
+    * for Laravel 5 compatibility
+    *
+    * @var string
+    */
+    public static $packageConfigSeparator = '::';
 
-	/**
-	 * The laravel version number. This is used for the install commands
-	 *
-	 * @var int
-	 */
-	public static $laravelVersion = 4;
+    /**
+    * The laravel version number. This is used for the install commands
+    *
+    * @var int
+    */
+    public static $laravelVersion = 4;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+    * Indicates if loading of the provider is deferred.
+    *
+    * @var bool
+    */
+    protected $defer = false;
 
-	/**
-	 * Boot the service provider.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+    * Boot the service provider.
+    *
+    * @return void
+    */
+    public function boot()
+    {
         /*
          * If the package method exists, we're using Laravel 4, if not, we're on 5
          */
-		if(method_exists($this, 'package'))
+        if(method_exists($this, 'package'))
         {
-			$this->package('stevebauman/inventory');
-		} else
+            $this->package('stevebauman/inventory');
+        } else
         {
-			$this::$packageConfigSeparator = '.';
+            $this::$packageConfigSeparator = '.';
 
-			$this::$laravelVersion = 5;
+            $this::$laravelVersion = 5;
 
-			$this->loadTranslationsFrom(__DIR__ . '/../../lang', 'inventory');
+            $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'inventory');
 
-			$this->publishes([
-				__DIR__ . '/../../config/config.php' => config_path('inventory.php'),
-			], 'config');
+            $this->publishes([
+                __DIR__ . '/../../config/config.php' => config_path('inventory.php'),
+            ], 'config');
 
-			$this->publishes([
-				__DIR__ . '/../../migrations/' => base_path('/database/migrations'),
-			], 'migrations');
-		}
-	}
+            $this->publishes([
+                __DIR__ . '/../../migrations/' => base_path('/database/migrations'),
+            ], 'migrations');
+        }
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+    * Register the service provider.
+    *
+    * @return void
+    */
+    public function register()
+    {
         /*
          * Bind the install command
          */
-		$this->app->bind('inventory:install', function() {
-			return new Commands\InstallCommand();
-		});
+        $this->app->bind('inventory:install', function()
+        {
+            return new Commands\InstallCommand();
+        });
 
         /*
          * Bind the check-schema command
          */
-		$this->app->bind('inventory:check-schema', function() {
-			return new Commands\SchemaCheckCommand();
-		});
+        $this->app->bind('inventory:check-schema', function()
+        {
+            return new Commands\SchemaCheckCommand();
+        });
 
         /*
          * Bind the run migrations command
          */
-		$this->app->bind('inventory:run-migrations', function() {
-			return new Commands\RunMigrationsCommand();
-		});
+        $this->app->bind('inventory:run-migrations', function()
+        {
+            return new Commands\RunMigrationsCommand();
+        });
 
         /*
          * Register the commands
          */
-		$this->commands(array(
-			'inventory:install',
-			'inventory:check-schema',
-			'inventory:run-migrations',
-		));
+        $this->commands(array(
+            'inventory:install',
+            'inventory:check-schema',
+            'inventory:run-migrations',
+        ));
 
         /*
          * Include the helpers file
          */
-		include __DIR__ .'/../../helpers.php';
-	}
+        include __DIR__ .'/../../helpers.php';
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('inventory');
-	}
+    /**
+    * Get the services provided by the provider.
+    *
+    * @return array
+    */
+    public function provides()
+    {
+        return array('inventory');
+    }
 
 }
