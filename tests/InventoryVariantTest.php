2f379a828c796586a0af771cf8dc7344cd193386
Steve Bauman <steven_bauman@outlook.com>
2015-05-05 12:10:44 -0400

    Added  createVariant test

--- tests/InventoryVariantTest.php
+++ tests/InventoryVariantTest.php
@@ -30,6 +30,36 @@ class InventoryVariantTest extends InventoryTest
         $this->assertEquals($chocolateMilk->metric_id, $milk->metric_id);
     }
 
+    public function testCreateVariant()
+    {
+        $this->newCategory();
+        $this->newMetric();
+
+        $coke = Inventory::create([
+            'name' => 'Coke',
+            'description' => 'Delicious Pop',
+            'metric_id' => 1,
+            'category_id' => 1,
+        ]);
+
+        DB::shouldReceive('beginTransaction')->once()->andReturn(true);
+        DB::shouldReceive('commit')->once()->andReturn(true);
+
+        Event::shouldReceive('fire')->once()->andReturn(true);
+
+        $name = 'Cherry Coke';
+        $description = 'Delicious Cherry Coke';
+
+        $cherryCoke = $coke->createVariant($name, $description);
+
+        $this->assertTrue($cherryCoke->isVariant());
+        $this->assertEquals(1, $cherryCoke->parent_id);
+        $this->assertEquals($name, $cherryCoke->name);
+        $this->assertEquals($description, $cherryCoke->description);
+        $this->assertEquals(1, $cherryCoke->category_id);
+        $this->assertEquals(1, $cherryCoke->metric_id);
+    }
+
     public function testMakeVariant()
     {
         $this->newCategory();
