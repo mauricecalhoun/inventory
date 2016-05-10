9693715f60a217d8ccfb3cfb7e55228a2f96227c
Steve Bauman <steven_bauman@outlook.com>
2016-05-10 09:11:56 -0400

    Make configuration prettier

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,57 +1,76 @@
 <?php
 
-/**
- * The Inventory configuration file.
- */
 return [
 
     /*
-     * Allows inventory changes to occur without a user responsible
-     *
-     * @var bool
-     */
+    |--------------------------------------------------------------------------
+    | Allow no user
+    |--------------------------------------------------------------------------
+    |
+    | Allows inventory changes to occur without a user responsible.
+    |
+    */
+
     'allow_no_user' => false,
 
     /*
-     * Allows inventory stock movements to have the same before and after quantity
-     *
-     * @var bool
-     */
+    |--------------------------------------------------------------------------
+    | Allow Duplicate Movements
+    |--------------------------------------------------------------------------
+    |
+    | Allows inventory stock movements to have the same before and after quantity.
+    |
+    */
+
     'allow_duplicate_movements' => true,
 
     /*
-     * When set to true, this will reverse the cost in the rolled back movement.
-     *
-     * For example, if the movement's cost that is being rolled back is 500, the rolled back
-     * movement will be -500.
-     *
-     * @var bool
-     */
+    |--------------------------------------------------------------------------
+    | Rollback Cost
+    |--------------------------------------------------------------------------
+    |
+    | For example, if the movement's cost that is being rolled
+    | back is 500, the rolled back movement will be -500.
+    |
+    */
+
     'rollback_cost' => true,
 
     /*
-     * Enables SKUs to be automatically generated on item creation
-     *
-     * @var bool
-     */
+    |--------------------------------------------------------------------------
+    | Skus Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Enables SKUs to be automatically generated on item creation.
+    |
+    */
+
     'skus_enabled' => true,
 
     /*
-     * The sku prefix length, not including the code for example:
-     *
-     * An item with a category named 'Sauce', the sku prefix generated will be: SAU
-     *
-     * @var int
-     */
+    |--------------------------------------------------------------------------
+    | Sku Prefix Length
+    |--------------------------------------------------------------------------
+    |
+    | The sku prefix length, not including the code for example:
+    |
+    | An item with a category named 'Sauce', the sku prefix generated will be: SAU
+    |
+    */
+
     'sku_prefix_length' => 3,
 
     /*
-     * The sku code length, not including prefix for example:
-     *
-     * An item with an ID of 1 (one) the sku code will be: 000001
-     *
-     * @var int
-     */
+    |--------------------------------------------------------------------------
+    | Sku Code Length
+    |--------------------------------------------------------------------------
+    |
+    | The sku code length, not including prefix for example:
+    |
+    | An item with an ID of 1 (one) the sku code will be: 000001
+    |
+    */
+
     'sku_code_length' => 6,
 
     /*
@@ -62,6 +81,19 @@ return [
      *
      * @var string
      */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sku Separator
+    |--------------------------------------------------------------------------
+    |
+    | The sku separator for use in separating the prefix from the code.
+    |
+    | For example, if a hyphen (-) is inserted in the string
+    | below, a possible SKU might be 'DRI-00001'
+    |
+    */
+
     'sku_separator' => '',
 
 ];
